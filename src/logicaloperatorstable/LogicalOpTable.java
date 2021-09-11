9e80057b809663b03b2177d9e6d76dc3257f8dfe
Adriano <adriano.henrique.eng@gmail.com>
2021-09-11 17:49:31 -0300

    The header was fixed and the first line was printed correctly.

--- src/logicaloperatorstable/LogicalOpTable.java
+++ src/logicaloperatorstable/LogicalOpTable.java
@@ -4,7 +4,9 @@ public class LogicalOpTable {
     public static void main(String[] args) {
         boolean x, y;
         System.out.println("TABLE OF LOGICAL OPERATORS-------------------");
-        System.out.println("X\t\tY\t\tAND\t\tOR\t\tXOR\t\tXNOR");
-
+        System.out.println("X\t\tY\t\tAND\t\tOR\t\tXOR\t\tNOT(X)\tNOT(Y)");
+        x = y = false;
+        System.out.print(x + "\t" + y + "\t" + (x & y) + "\t" + (x | y) + "\t");
+        System.out.println((x ^ y) + "\t" + !(x) + "\t" + !(y));
     }
 }
