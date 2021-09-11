6ed1ac9cd473affd48e19deca5f80b4734e2e1c2
Adriano <adriano.henrique.eng@gmail.com>
2021-09-11 17:56:02 -0300

    The lines 2 to 4 were added; now the table displays the outcome of each operation under its proper heading.

--- src/logicaloperatorstable/LogicalOpTable.java
+++ src/logicaloperatorstable/LogicalOpTable.java
@@ -3,10 +3,19 @@ package logicaloperatorstable;
 public class LogicalOpTable {
     public static void main(String[] args) {
         boolean x, y;
-        System.out.println("TABLE OF LOGICAL OPERATORS-------------------");
+        System.out.println("TABLE OF LOGICAL OPERATORS----------------------------");
         System.out.println("X\t\tY\t\tAND\t\tOR\t\tXOR\t\tNOT(X)\tNOT(Y)");
         x = y = false;
-        System.out.print(x + "\t" + y + "\t" + (x & y) + "\t" + (x | y) + "\t");
+        System.out.print(x + "\t" + y + "\t" + (x && y) + "\t" + (x || y) + "\t");
+        System.out.println((x ^ y) + "\t" + !(x) + "\t" + !(y));
+        x = false; y = true;
+        System.out.print(x + "\t" + y + "\t" + (x && y) + "\t" + (x || y) + "\t");
+        System.out.println((x ^ y) + "\t" + !(x) + "\t" + !(y));
+        x = true; y = false;
+        System.out.print(x + "\t" + y + "\t" + (x && y) + "\t" + (x || y) + "\t");
+        System.out.println((x ^ y) + "\t" + !(x) + "\t" + !(y));
+        x = y = true;
+        System.out.print(x + "\t" + y + "\t" + (x && y) + "\t" + (x || y) + "\t");
         System.out.println((x ^ y) + "\t" + !(x) + "\t" + !(y));
     }
 }
