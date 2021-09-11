9346acce729158e91d4f4ff5bdcde2861b898b7f
Adriano <adriano.henrique.eng@gmail.com>
2021-09-11 17:33:47 -0300

    Initial commit

---
+++ src/logicaloperatorstable/LogicalOpTable.java
@@ -0,0 +1,10 @@
+package logicaloperatorstable;
+
+public class LogicalOpTable {
+    public static void main(String[] args) {
+        boolean x, y;
+        System.out.println("TABLE OF LOGICAL OPERATORS-------------------");
+        System.out.println("X\t\tY\t\tAND\t\tOR\t\tXOR\t\tXNOR");
+
+    }
+}
